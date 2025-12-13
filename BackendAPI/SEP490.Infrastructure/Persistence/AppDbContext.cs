3c50c0250bfd7ecbed585af2fa261c6ae0265004
LamNV290304 <nguyenvietlam290304@gmail.com>
2025-12-13 21:22:39 +0700

    Update AppDbcontext

--- BackendAPI/SEP490.Infrastructure/Persistence/AppDbContext.cs
+++ BackendAPI/SEP490.Infrastructure/Persistence/AppDbContext.cs
@@ -15,14 +15,17 @@ namespace SEP490.Infrastructure.Persistence
 
         public DbSet<User> Users => Set<User>();
 
+        #region Logging
         /// <summary>
         /// Logging config
-        /// !!!WARNING
+        /// !!!WARNING, DO NOT DELETE THIS SECTION!!! 
+        /// If you delete this section, the logging feature will be broken.
         /// </summary>
         public DbSet<LogError> LogErrors => Set<LogError>();
         public DbSet<LogActivity> LogActivities => Set<LogActivity>();
         public DbSet<LogHistory> LogHistories => Set<LogHistory>();
         public DbSet<LogLogin> LogLogins => Set<LogLogin>();
+        #endregion
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
