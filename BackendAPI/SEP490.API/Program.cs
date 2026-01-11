cb47a5f37919f8876bcc80ce7fc6b5175fae9390
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-11 23:06:37 +0700

    Clean file

--- BackendAPI/SEP490.API/Program.cs
+++ BackendAPI/SEP490.API/Program.cs
@@ -11,7 +11,6 @@ namespace SEP490.API
             builder.Services.AddApplication();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
