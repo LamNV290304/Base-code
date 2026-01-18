35c2701f3baddc6c05f36bafc6ecd4a8ea8cd29a
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-18 17:04:18 +0700

    Clean comment

--- BackendAPI/SEP490.API/Program.cs
+++ BackendAPI/SEP490.API/Program.cs
@@ -9,19 +9,16 @@ namespace SEP490.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // 1. Thêm dịch vụ Rate Limiting
             builder.Services.AddRateLimiter(options =>
             {
-                // Cấu hình chính sách "fixed" (cố định)
                 options.AddFixedWindowLimiter(policyName: "fixed", opt =>
                 {
-                    opt.PermitLimit = 10; // Tối đa 10 yêu cầu
-                    opt.Window = TimeSpan.FromSeconds(10); // Trong vòng 10 giây
+                    opt.PermitLimit = 10; 
+                    opt.Window = TimeSpan.FromSeconds(10); 
                     opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-                    opt.QueueLimit = 2; // Số lượng yêu cầu chờ trong hàng đợi
+                    opt.QueueLimit = 2; 
                 });
 
-                // Cấu hình phản hồi khi bị giới hạn (429 Too Many Requests)
                 options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
             });
 
